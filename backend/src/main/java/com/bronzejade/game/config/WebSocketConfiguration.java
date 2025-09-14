4b7b11ebe8878c77393adb5e9abc255ac39a3ee4
RomaZinkevich <zra1903@mail.ru>
2025-09-14 19:37:11 +0300

    refactored config file

--- backend/src/main/java/com/bronzejade/game/config/WebSocketConfiguration.java
+++ backend/src/main/java/com/bronzejade/game/config/WebSocketConfiguration.java
@@ -1,4 +1,4 @@
-package com.bronzejade.game;
+package com.bronzejade.game.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
