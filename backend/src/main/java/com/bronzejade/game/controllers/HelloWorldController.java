3e9cb428273a769c9b21b2e1f9eb497dc30f9f5e
Roman Zinkevich <75644570+RomaZinkevich@users.noreply.github.com>
2025-09-14 19:48:51 +0300

    Revert "testing websockets"

--- backend/src/main/java/com/bronzejade/game/controllers/HelloWorldController.java
+++ backend/src/main/java/com/bronzejade/game/controllers/HelloWorldController.java
@@ -1,8 +1,6 @@
 package com.bronzejade.game.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,19 +10,5 @@ public class HelloWorldController {
     @GetMapping
     public ResponseEntity<String> helloWorld() {
         return ResponseEntity.ok("Hello World");
-    }
-
-    // WebSocket message handler
-    @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public String handleMessage(String message) {
-        return "Hello, " + message + "!";
-    }
-
-    // WebSocket broadcast handler
-    @MessageMapping("/broadcast")
-    @SendTo("/topic/greetings")
-    public String handleBroadcast(String message) {
-        return "Broadcast: " + message;
     }
 }
