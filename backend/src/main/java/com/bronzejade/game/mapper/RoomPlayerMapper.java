82e6d99a9355663e195ae1372ac31c36448d6f48
RomaZinkevich <zra1903@mail.ru>
2025-12-04 00:46:39 +0200

    fixed null user id in start game response dto

--- backend/src/main/java/com/bronzejade/game/mapper/RoomPlayerMapper.java
+++ backend/src/main/java/com/bronzejade/game/mapper/RoomPlayerMapper.java
@@ -3,9 +3,13 @@ package com.bronzejade.game.mapper;
 import com.bronzejade.game.domain.dtos.User.RoomPlayerDto;
 import com.bronzejade.game.domain.entities.RoomPlayer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RoomPlayerMapper {
+
+    @Mapping(target = "userId", source = "user.id")
+    @Mapping(target = "guest", expression = "java(roomPlayer.isGuest())")
     RoomPlayerDto toDto(RoomPlayer roomPlayer);
 }
