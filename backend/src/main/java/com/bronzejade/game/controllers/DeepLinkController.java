453c51d19ed6fa507297dc1e5620a3fcc6eaa91d
Prabesh Sharma <prwshshrm@gmail.com>
2025-12-06 21:16:51 +0200

    fix deeplink controller

--- backend/src/main/java/com/bronzejade/game/controllers/DeepLinkController.java
+++ backend/src/main/java/com/bronzejade/game/controllers/DeepLinkController.java
@@ -1,9 +1,14 @@
 package com.bronzejade.game.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
 public class DeepLinkController {
   @GetMapping("/join")
   public String joinPage(@RequestParam String code, Model model) {
